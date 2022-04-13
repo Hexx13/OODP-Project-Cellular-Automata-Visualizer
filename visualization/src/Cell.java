135947e15c0bdff7227279cf02f223673edb07f8
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-04-13 15:12:30 +0100

    Add colour logic

--- visualization/src/Cell.java
+++ visualization/src/Cell.java
@@ -1,16 +1,34 @@
 public class Cell extends ClonableCell{
 
     private char cellState;
+    private String colour;
+
 
     public Cell(char cellState) {
         this.cellState = cellState;
     }
 
-    public ClonableCell makeClone(){
+    private void Colour(){
+        if(cellState == 'A'){
+            this.colour = "green";
+        }
+        else if(cellState == 'C'){
+            this.colour = "red";
+        }
+        else if(cellState == 'D'){
+            this.colour = "black";
+        }
+        else if(cellState > 70 && cellState <= 89){
+            this.colour = "blue";
+        }
+
+    }
+
+    public Cell makeClone(){
         //Variable where reference is stored;
         Cell cellObject = null;
         try{
-            cellObject = (Cell)super.clone();
+            cellObject = (Cell) super.clone();
         }
         catch (CloneNotSupportedException e){
             e.printStackTrace();
